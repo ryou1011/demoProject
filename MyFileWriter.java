bb9bed76b07ee5435b8c0802a40ce9c2fbc1cee9
Ryan You <115812707+bbysitterr@users.noreply.github.com>
2024-09-07 20:30:02 -0700

    homework 1
    done

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,50 +1,27 @@
-#hfuddds
+
 import java.io.*;
 import java.nio.file.*;
-import java.nio.charset.StandardCharsets;
 
-public class FileWriteExample {
-    public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example1.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
+public class MyFileWriter {
 
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        String hiddenFileName = ".secretpassword.txt";
+        String password = "SuperSecretPassword123!";
+        String hiddenFolder = ".classified";
+        String regularFileName = "confidential.dat";
+        String secretData = "Top Secret Data: Highly Confidential.";
+
+        try ( // 1. Creating a hidden file with a password
+                FileWriter hiddenFileWriter = new FileWriter(hiddenFileName)) {
+            hiddenFileWriter.write(password);
         }
 
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // 2. Creating a hidden folder and writing a regular file inside it
+        Path hiddenFolderPath = Paths.get(hiddenFolder);
+        Files.createDirectory(hiddenFolderPath);
 
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            e.printStackTrace();
+        try (FileWriter regularFileWriter = new FileWriter(hiddenFolder + "/" + regularFileName)) {
+            regularFileWriter.write(secretData);
         }
     }
 }
